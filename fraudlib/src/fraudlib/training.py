e83119d0f9812ea05158d8870c608f9635b86566
Jessy Wang <wwwwww04210107@gmail.com>
2026-08-15 17:37:22 +0100

    Update: amendment on training features

--- fraudlib/src/fraudlib/training.py
+++ fraudlib/src/fraudlib/training.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import fraudlib.features as features
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
@@ -5,16 +6,23 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import numpy as np
 
+TARGET_LABEL = 'is_fraud'
+
+NUMERIC_FEATURES = ['amount', 'amount_mean_7d', 'amount_std_7d', 'amount_max_7d', 'amount_min_7d', 'amount_count_7d']
+CATEGORICAL_FEATURES = ['channel']
+
 
 def train_test_split(df):
     """
     Split the data into train and test based on time.
     """
+    cutoff = df['ts'].min() + pd.Timedelta(days=60)
+
     ## Day 1 - 60
-    train_df = df[df['ts'] <= np.percentile(df.ts, 90/100*60)]
+    train_df = df[df['ts'] <= cutoff]
 
     ## Day 61 - 90
-    test_df = df[df['ts'] > np.percentile(df.ts, 90/100*60)]
+    test_df = df[df['ts'] > cutoff]
 
     ## Random Split might cheat the model because we are forcasting time-series data, therefore, we should split the data based on time to avoid data leakage.
 
@@ -24,8 +32,8 @@ def train_test_split(df):
 def create_preprocessor():
     preprocessor = ColumnTransformer(
         transformers=[
-            ('num', StandardScaler(), ['amount']),
-            ('cat', OneHotEncoder(drop='first'), ['is_fraud'])
+            ('num', StandardScaler(), NUMERIC_FEATURES),
+            ('cat', OneHotEncoder(drop='first'), CATEGORICAL_FEATURES)
         ]
     )
     return preprocessor
@@ -41,12 +49,12 @@ def train_model(train_df, test_df):
         ('classifier', lr)
     ])
 
-    pipeline.fit(train_df[['amount', 'is_fraud']], train_df['is_fraud'])
+    pipeline.fit(train_df[NUMERIC_FEATURES + CATEGORICAL_FEATURES], train_df[TARGET_LABEL])
     return pipeline
 
 def evaluate_model(pipeline, test_df):
-    X_test = test_df[['amount', 'is_fraud']]
-    y_test = test_df['is_fraud']
+    X_test = test_df[NUMERIC_FEATURES + CATEGORICAL_FEATURES]
+    y_test = test_df[TARGET_LABEL]
 
     y_pred = pipeline.predict(X_test)
 
